3ff4a6411ea8f58bd6d1d824f5fd99229d3a75ea
MaxMakela <johnhopeful11@gmai.com>
2017-12-30 18:54:59 +0200

    ver 0.3

--- app/src/main/java/com/jarvis/coockiesforwords/StudyingActivity.java
+++ app/src/main/java/com/jarvis/coockiesforwords/StudyingActivity.java
@@ -1,18 +1,39 @@
 package com.jarvis.coockiesforwords;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class StudyingActivity extends AppCompatActivity {
+public class StudyingActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button newWord,answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studying);
+
+        newWord = (Button) findViewById(R.id.newWord);
+        newWord.setOnClickListener(this);
+
+        answer = (Button) findViewById(R.id.answer);
+        answer.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+
+            case R.id.answer:
+
+                break;
+            case R.id.newWord:
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+                break;
+
+       }
+    }
 }
